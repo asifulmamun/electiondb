a37ee882e094fce552cb362770b688ccd32ef119
Al Mamun <asifulmamun@users.noreply.github.com>
2020-02-01 12:54:22 +0600

    nnn

--- dob.php
+++ dob.php
@@ -30,7 +30,7 @@
             $name = $_POST['name'];
             $dob = $_POST['dob'];
             // Query Which Result Want
-            $sql = "SELECT * FROM voter_lists WHERE dob LIKE '%$dob%'";
+            $sql = "SELECT * FROM voter_lists WHERE dob LIKE '%$dob%' AND name LIKE '%$name%'";
             // DB Data Search Query Execute
             $results = $db->conn->query($sql);
 
